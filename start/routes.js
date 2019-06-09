21f88518ee9de1dec1fda86b69cfd95e7888ab00
Darlan Caruso <devcaruso@gmail.com>
2019-06-09 13:53:38 -0300

    Project CRUD

--- start/routes.js
+++ start/routes.js
@@ -2,12 +2,18 @@
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
-
+// User
 Route.post('users', 'UserController.store')
 Route.post('sessions', 'SessionController.store')
-
+// Session
 Route.post('passwords', 'ForgotPasswordController.store')
 Route.put('passwords', 'ForgotPasswordController.update')
-
-Route.post('file', 'FileController.store')
+// Auth
+Route.group(() => {
+  // File Create
+  Route.post('file', 'FileController.store')
+  // Project
+  Route.resource('projects', 'ProjectController').apiOnly()
+}).middleware(['auth'])
+// File Show
 Route.get('file/:id', 'FileController.show')
