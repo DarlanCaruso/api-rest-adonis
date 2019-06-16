63b04b4f379e1d08b4e909e1951a3dee08544cb8
Darlan Caruso <devcaruso@gmail.com>
2019-06-15 21:55:55 -0300

    Configurating kue

--- app/Models/Hooks/TaskHook.js
+++ app/Models/Hooks/TaskHook.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const Mail = use('Mail')
-const Helpers = use('Helpers')
+const Kue = use('Kue')
+const Job = use('App/Jobs/NewTaskMail')
 const TaskHook = (exports = module.exports = {})
 
 TaskHook.sendNewTaskMail = async taskInstance => {
@@ -11,24 +11,5 @@ TaskHook.sendNewTaskMail = async taskInstance => {
   const file = await taskInstance.file().fetch()
   const { title } = taskInstance
 
-  await Mail.send(
-    ['emails.new_task'],
-    {
-      username,
-      title,
-      hasAttachment: !!file
-    },
-    message => {
-      message
-        .to(email)
-        .from('devcaruso@gmail.com')
-        .subject('Nova tarefa para você')
-
-      if (file) {
-        message.attach(Helpers.tmpPath(`uploads/${file.file}`), {
-          filename: file.name
-        })
-      }
-    }
-  )
+  Kue.dispatch(Job.key, { username, email, file, title }, { attempts: 3 })
 }
